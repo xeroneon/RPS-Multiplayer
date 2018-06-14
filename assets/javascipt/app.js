78960eda3f764a3a1a79398128b0a41491b5d978
Xeroneon <andrewvasquez0113@gmail.com>
2018-06-13 21:07:06 -0700

    connected to firebase and added player 1 and 2 prompts

---
+++ assets/javascipt/app.js
@@ -0,0 +1,80 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAVq_eX-wS_RN8O8dpyC-a-cSQ_TZBXn64",
+    authDomain: "rps-mujltiplayer.firebaseapp.com",
+    databaseURL: "https://rps-mujltiplayer.firebaseio.com",
+    projectId: "rps-mujltiplayer",
+    storageBucket: "",
+    messagingSenderId: "5114991892"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var player1Name = "";
+
+var player2Name = "";
+
+var player1Choice = "";
+
+var player2Choice = "";
+
+
+// connectionsRef references a specific location in our database.
+// All of our connections will be stored in this directory.
+var connectionsRef = database.ref("/connections");
+
+// '.info/connected' is a special location provided by Firebase that is updated
+// every time the client's connection state changes.
+// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+var connectedRef = database.ref(".info/connected");
+
+
+
+connectedRef.on("value", function (snap) {
+
+    // If they are connected..
+    if (snap.val()) {
+
+        // Add user to the connections list.
+        var con = connectionsRef.push(true);
+        // Remove user from the connection list when they disconnect.
+        con.onDisconnect().remove();
+    }
+
+
+});
+
+$("#player1-submit").on("click", function() {
+    player1Name = $("#player1-input").val();
+
+    database.ref().set({
+        player1Name: player1Name
+    });
+
+})
+
+$("#player2-submit").on("click", function() {
+    player2Name = $("#player2-input").val();
+
+    database.ref().set({
+        player2Name: player2Name
+    });
+
+})
+
+database.ref().on("value", function(snap) {
+    
+    // if (snapshot.child("player1Name").exists || snapshot.child("player2Name").exists ) {
+        player1Name = snap.val().player1Name
+        player2Name = snap.val().player2Name
+        $("#player1-name").text(player1Name);
+        $("#player2-name").text(player2Name);
+
+    // } else {
+    //     $("#player1-name").text("Player 1");
+    //     $("#player2-name").text("Player 2");
+    // }
+
+})
