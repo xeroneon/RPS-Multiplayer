f867989a5cdc9bc8619eeef46a505acde1a2b8be
Xeroneon <andrewvasquez0113@gmail.com>
2018-06-15 21:57:23 -0700

    i have no idea

--- assets/javascipt/app.js
+++ assets/javascipt/app.js
@@ -33,19 +33,33 @@ var connectionsRef = database.ref("/connections");
 // '.info/connected' is a boolean value, true if the client is connected and false if they are not.
 var connectedRef = database.ref(".info/connected");
 
+// console.log(connectedRef);
+
 connectedRef.on("value", function (snap) {
 
     // If they are connected..
     if (snap.val()) {
-
+        // console.log(snap.name());
         // Add user to the connections list.
-        var con = connectionsRef.push(true);
+        var con = connectionsRef.push(true)
+        // .then((snap) => {
+        //     var key = snap.key;
+        //     console.log(key);
+        //  });
         // Remove user from the connection list when they disconnect.
-        con.onDisconnect().remove();
+
+        con.update({
+            player1Connected: true
+        })
+
+        con.onDisconnect().remove().update({
+            player1Connected: false
+        });
 
         // if (snap.numChildren() === 0) {
         //     player1Name = prompt("What is your name?");
         // }
+
     }
 
 
@@ -62,6 +76,12 @@ connectionsRef.on("value", function (snap) {
         // database.ref("/players").set({
         //     player1Name: player1Name
         // });
+
+        playersCon = 1;
+
+        database.ref().update({
+            playersCon: playersCon
+        })
     }
     if (snap.numChildren() === 2) {
         // player2Name = prompt("What is your name?");
@@ -69,32 +89,50 @@ connectionsRef.on("value", function (snap) {
         // database.ref("/players").set({
         //     player2Name: player2Name
         // });
-        if (sessionStorage.getItem("isPlayer") === player1Name) {
-            $("#player1-choice").text("Rock");
-        }
+        // if (sessionStorage.getItem("isPlayer") === "1") {
+        //     $("#player1-choice").text("Rock");
+        // }
+
+        playersCon = 2;
+
+        database.ref().update({
+            playersCon: playersCon
+        })
+    }
+    // connectionsRef.update({
+    //     player1Connected: false
+    // })
+    if (con.val().player1Connected === false) {
+        con.update({
+            player1Connected: true
+        })
     }
 
-    player1Name = snap.parent().val().player1Name;
-    player2Name = snap.parent().val().player2Name;
 })
 
 $("#player1-submit").on("click", function () {
     player1Name = $("#player1-input").val();
     player2Name = $("#player2-input").val();
 
+    var player1Ready = true;
+
     database.ref("/players").update({
-        player1Name: player1Name
+        player1Name: player1Name,
+        player1Ready: player1Ready
     });
 
-    sessionStorage.setItem("isPlayer", player1Name);
+    sessionStorage.setItem("isPlayer", "1");
 })
 
 $("#player2-submit").on("click", function () {
     player1Name = $("#player1-input").val();
     player2Name = $("#player2-input").val();
 
+    var player2Ready = true;
+
     database.ref("/players").update({
-        player2Name: player2Name
+        player2Name: player2Name,
+        player2Ready: player2Ready
     });
 
     sessionStorage.setItem("isPlayer", "2");
@@ -109,11 +147,25 @@ database.ref("/players").on("value", function (snap) {
     $("#player1-name").text(player1Name);
     $("#player2-name").text(player2Name);
 
+    player1Ready = snap.val().player1Ready;
+    player2Ready = snap.val().player2Ready;
+
     // } else {
     //     $("#player1-name").text("Player 1");
     //     $("#player2-name").text("Player 2");
     // }
 
-    playersCon = snap.val().playersCon;
+    // playersCon = snap.val().playersCon;
+
+
 
 })
+
+if (playersCon === 2) {
+    if (sessionStorage.getItem("isPlayer") === "1") {
+        console.log("ran");
+        $("#rock1").text("Rock");
+        $("#paper1").text("Paper");
+        $("#scissors1").text("Scissors");
+    }
+}
