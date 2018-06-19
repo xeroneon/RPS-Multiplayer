8b804a8092a9077c16cf764810947bcd148ba335
Xeroneon <andrewvasquez0113@gmail.com>
2018-06-18 18:37:57 -0700

    added choices and who won, need to restart game

--- assets/javascipt/app.js
+++ assets/javascipt/app.js
@@ -162,7 +162,7 @@ connectionsRef.on("value", function (snap) {
     //     })
     // }
 
-})
+});
 
 $("#player1-submit").on("click", function () {
     player1Name = $("#player1-input").val();
@@ -181,7 +181,7 @@ $("#player1-submit").on("click", function () {
     $("#player2-input").addClass("hide");
     $("#player1-submit").addClass("hide");
     $("#player2-submit").addClass("hide");
-})
+});
 
 $("#player2-submit").on("click", function () {
     player1Name = $("#player1-input").val();
@@ -200,7 +200,7 @@ $("#player2-submit").on("click", function () {
     $("#player1-submit").addClass("hide");
     $("#player2-submit").addClass("hide");
 
-})
+});
 
 database.ref("/players").on("value", function (snap) {
 
@@ -222,7 +222,7 @@ database.ref("/players").on("value", function (snap) {
 
 
 
-})
+});
 
 database.ref().on("value", function(snap) {
     player2Connected = snap.val().player2Connected;
@@ -233,7 +233,9 @@ database.ref().on("value", function(snap) {
 
     // console.log(player1Connected);
     if (player1Connected === true && player2Connected === true) {
-        if (sessionStorage.getItem("isPlayer") === "1") {
+        // sessionStorage.setItem("chose", false);
+        
+        if (sessionStorage.getItem("isPlayer") === "1" && snap.val().player1chose === false) {
             console.log("ran");
 
             $("#rock1").removeClass("hide");
@@ -241,24 +243,140 @@ database.ref().on("value", function(snap) {
             $("#scissors1").removeClass("hide");
         }
     }
+
+    if (snap.val().player1chose === true && snap.val().player2chose === true) {
+        if (snap.val().player1Choice === "rock" && snap.val().player2Choice === "rock") {
+            // console.log("result");
+            $("#result").text("Tie");
+        }
+        else if (snap.val().player1Choice === "rock" && snap.val().player2Choice === "paper") {
+            // console.log("result");
+            $("#result").text(player2Name + " Wins");
+        }
+        else if (snap.val().player1Choice === "rock" && snap.val().player2Choice === "scissors") {
+            // console.log("result");
+            $("#result").text(player1Name + " Wins");
+        }
+
+        else if (snap.val().player1Choice === "paper" && snap.val().player2Choice === "rock") {
+            // console.log("result");
+            $("#result").text(player1Name + " Wins");
+        }
+        else if (snap.val().player1Choice === "scissors" && snap.val().player2Choice === "rock") {
+            // console.log("result");
+            $("#result").text(player2Name + " Wins");
+        }
+    }
+});
+
+database.ref("player1Choice").on("value", function(snap) {
+    if (sessionStorage.getItem("isPlayer") === "2") {
+        $("#rock2").removeClass("hide");
+        $("#paper2").removeClass("hide");
+        $("#scissors2").removeClass("hide");
+    }
+});
+
+// player 1 choices
+database.ref().update({
+    player1Choice: "",
+    player1chose: false
 })
-
-
 $("#rock1").on("click", function() {
-    player1Choice = "rock"
+    player1Choice = "rock";
+    database.ref().update({
+        player1Choice: "",
+        player1chose: true
+    });
     database.ref().update({
         player1Choice: player1Choice
-    })
+    });
+    if (sessionStorage.getItem("isPlayer") === "1") {
+        $("#rock1").addClass("hide");
+        $("#paper1").addClass("hide");
+        $("#scissors1").addClass("hide");
+    }
+    console.log("click");
 } )
 $("#paper1").on("click", function() {
-    player1Choice = "paper"
+    player1Choice = "paper";
+    database.ref().update({
+        player1Choice: "",
+        player1chose: true
+    });
     database.ref().update({
         player1Choice: player1Choice
-    })
+    });
+    console.log("click");
 } )
 $("#scissors1").on("click", function() {
-    player1Choice = "scissors"
+    player1Choice = "scissors";
+    database.ref().update({
+        player1Choice: "",
+        player1chose: true
+    });
     database.ref().update({
         player1Choice: player1Choice
-    })
+    });
+    console.log("click");
 } )
+
+
+//player 2 choices
+database.ref().update({
+    player2Choice: "",
+    player2chose: false
+})
+$("#rock2").on("click", function() {
+    player2Choice = "rock";
+    database.ref().update({
+        player2Choice: "",
+        player2chose: true
+    });
+    database.ref().update({
+        player2Choice: player2Choice
+    });
+    if (sessionStorage.getItem("isPlayer") === "2") {
+        $("#rock2").addClass("hide");
+        $("#paper2").addClass("hide");
+        $("#scissors2").addClass("hide");
+    }
+    console.log("click");
+} )
+$("#paper2").on("click", function() {
+    player2Choice = "paper";
+    database.ref().update({
+        player2Choice: "",
+        player2chose: true
+    });
+    database.ref().update({
+        player2Choice: player2Choice
+    });
+    if (sessionStorage.getItem("isPlayer") === "2") {
+        $("#rock2").addClass("hide");
+        $("#paper2").addClass("hide");
+        $("#scissors2").addClass("hide");
+    }
+    console.log("click");
+} )
+$("#scissors2").on("click", function() {
+    player2Choice = "scissors";
+    database.ref().update({
+        player2Choice: "",
+        player2chose: true
+    });
+    database.ref().update({
+        player2Choice: player2Choice
+    });
+    if (sessionStorage.getItem("isPlayer") === "2") {
+        $("#rock2").addClass("hide");
+        $("#paper2").addClass("hide");
+        $("#scissors2").addClass("hide");
+    }
+    console.log("click");
+} )
+
+//calculate who wins
+database.ref("player2Choice").on("value", function(snap) {
+
+})
